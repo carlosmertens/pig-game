efbc988e6d332985f830b5d583c7718055ff6b09
Carlos Mertens <mertenscarlos@yahoo.com>
2021-05-10 21:48:06 +0200

    Set initial values

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Select elements
+const score0 = document.getElementById('score--0');
+const score1 = document.getElementById('score--1');
+const dice = document.querySelector('.dice');
+
+// Set initial values
+score0.textContent = 0;
+score1.textContent = 0;
+dice.classList.add('hidden');
